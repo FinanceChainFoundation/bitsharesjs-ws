ce17720b22ac8e4e38e1a1fd0a07a955d8842148
pure-gui <182417746@qq.com>
2017-11-22 12:15:20 +0800

    delete ignore es and cjs

---
+++ es/src/ChainConfig.js
@@ -0,0 +1,77 @@
+var _this = void 0;
+
+var ecc_config = {
+    address_prefix: process.env.npm_config__graphene_ecc_default_address_prefix || "GPH"
+};
+
+_this = {
+    core_asset: "FCC",
+    address_prefix: "FCC",
+    expire_in_secs: 15,
+    expire_in_secs_proposal: 24 * 60 * 60,
+    review_in_secs_committee: 24 * 60 * 60,
+    networks: {
+        FinChain: {
+            core_asset: "FCC",
+            address_prefix: "FCC",
+            chain_id: "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8"
+        }
+    },
+
+    /** Set a few properties for known chain IDs. */
+    setChainId: function setChainId(chain_id) {
+
+        var i = void 0,
+            len = void 0,
+            network = void 0,
+            network_name = void 0,
+            ref = void 0;
+        ref = Object.keys(_this.networks);
+
+        for (i = 0, len = ref.length; i < len; i++) {
+
+            network_name = ref[i];
+            network = _this.networks[network_name];
+
+            if (network.chain_id === chain_id) {
+
+                _this.network_name = network_name;
+
+                if (network.address_prefix) {
+                    _this.address_prefix = network.address_prefix;
+                    ecc_config.address_prefix = network.address_prefix;
+                }
+
+                // console.log("INFO    Configured for", network_name, ":", network.core_asset, "\n");
+
+                return {
+                    network_name: network_name,
+                    network: network
+                };
+            }
+        }
+
+        if (!_this.network_name) {
+            console.log("Unknown chain id (this may be a testnet)", chain_id);
+        }
+    },
+
+    reset: function reset() {
+        _this.core_asset = "CORE";
+        _this.address_prefix = "GPH";
+        ecc_config.address_prefix = "GPH";
+        _this.expire_in_secs = 15;
+        _this.expire_in_secs_proposal = 24 * 60 * 60;
+
+        console.log("Chain config reset");
+    },
+
+    setPrefix: function setPrefix() {
+        var prefix = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "GPH";
+
+        _this.address_prefix = prefix;
+        ecc_config.address_prefix = prefix;
+    }
+};
+
+export default _this;
