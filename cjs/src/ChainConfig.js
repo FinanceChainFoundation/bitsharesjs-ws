fe97a2f7a5fce12d1dc80fee01f35762ec3fd0f0
pure-gui <182417746@qq.com>
2017-12-07 22:15:08 +0800

    change core asset name and address_prefix

--- cjs/src/ChainConfig.js
+++ cjs/src/ChainConfig.js
@@ -8,15 +8,15 @@ var ecc_config = {
 };
 
 _this = {
-    core_asset: "FCC",
-    address_prefix: "FCC",
+    core_asset: "JRC",
+    address_prefix: "JRC",
     expire_in_secs: 15,
     expire_in_secs_proposal: 24 * 60 * 60,
     review_in_secs_committee: 24 * 60 * 60,
     networks: {
         FinChain: {
-            core_asset: "FCC",
-            address_prefix: "FCC",
+            core_asset: "JRC",
+            address_prefix: "JRC",
             chain_id: "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8"
         }
     },
